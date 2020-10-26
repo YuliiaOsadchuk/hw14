d0538b7854e47fd5549fc1ef774ce2b1fc329c71
YuliiaOsadchuk <yulia.zelenskaya@gmail.com>
2020-10-26 12:52:35 +0200

    added carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('test');
+
+$(document).ready(function() {
+    $('.review-slider').slick({
+        arrows: true,
+        prevArrow: ".icon-arrow-left",
+        nextArrow: ".icon-arrow-right",
+    });
+});
